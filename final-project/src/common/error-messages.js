596654c52a6f274b914e86e96b5af059d3a84022
gerganadq <gerganaquievy@gmail.com>
2023-08-21 00:52:20 +0300

    Add error-messages.js constants

---
+++ final-project/src/common/error-messages.js
@@ -0,0 +1,18 @@
+export const errorMessages = {
+
+    USER_EXISTS: "This username already exists!",
+    PHONE_EXISTS: "This phone already exists!",
+    EMAIL_EXISTS: "This email already exists!",
+    SOMETHING_WENT_WRONG: "Something went wrong!",
+    USER_NOT_EXISTS: "User with this email doesn't exist!",
+    INVALID_EMAIL: "Invalid email!",
+    WRONG_PASSWORD: "Wrong password!",
+    ENTER_CORRECT_DATA: "Еnter the correct email and password!",
+    INVALID_DATA: "Please enter valid information!",
+    ONLY_LETTERS: "This field must contains only uppercase and lowercase letters!",
+    INPUT_VALUE: "Input a value!",
+    PHONE_LENGTH: "Phone number must have 10 digit!",
+    ONLY_DIGITS: "This field number must have only digits!",
+
+}
+
