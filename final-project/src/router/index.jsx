593b02f6905f1a39b3994192e073723a1fba795d
gerganadq <gerganaquievy@gmail.com>
2023-08-21 19:15:40 +0300

    Add LandingLayout in Index.jsx

--- final-project/src/router/index.jsx
+++ final-project/src/router/index.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
-import HomePage from "../pages/Home/HomePage"
 import LoginPage from "../pages/Login/LoginPage"
 import RegisterPage from "../pages/Register/RegisterPage"
+import LandingLayout from "../layout/LandingLayout"
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <LandingLayout />,
   },
   {
     path: "/login",
