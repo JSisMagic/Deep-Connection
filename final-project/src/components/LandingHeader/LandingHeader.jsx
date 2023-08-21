65bab873a7a5f3f4480e9adb6dda68742d243133
gerganadq <gerganaquievy@gmail.com>
2023-08-21 19:08:45 +0300

    Add LandingHeader.jsx

---
+++ final-project/src/components/LandingHeader/LandingHeader.jsx
@@ -0,0 +1,21 @@
+import { Box } from "@chakra-ui/react"
+
+const LandingHeader = () => {
+  return (
+    <Box
+      height="50px"
+      padding="1rem"
+      position="absolute"
+      top={0}
+      right={0}
+      left={0}
+      zIndex={2}
+      // bg="rgba(255,255,255, .1)"
+      // boxShadow="0px 4px 12px rgba(0,0,0,.05)"
+    >
+      Header
+    </Box>
+  )
+}
+
+export default LandingHeader
