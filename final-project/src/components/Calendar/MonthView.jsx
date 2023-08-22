76f35e7c1a2175e48d2a62dccc8164b9a230b0b5
gerganadq <gerganaquievy@gmail.com>
2023-08-22 19:10:52 +0300

    UI Improvement: Month Grid and Day Styling

--- final-project/src/components/Calendar/MonthView.jsx
+++ final-project/src/components/Calendar/MonthView.jsx
@@ -1,22 +1,39 @@
-import { Grid, Box } from "@chakra-ui/react";
-import { getStartOfMonth, getEndOfMonth, geteachDayOfInterval } from "../../services/calendar.services";
-import format from "date-fns/format";
-
+import { Grid, Box } from "@chakra-ui/react"
+import {
+  getStartOfMonth,
+  getEndOfMonth,
+  geteachDayOfInterval,
+} from "../../services/calendar.services"
+import format from "date-fns/format"
+import { COOL_PURPLE } from "../../common/colors"
 
 const MonthView = ({ date }) => {
-  const startOfMonth = getStartOfMonth(date);
-  const endOfMonth = getEndOfMonth(date);
-  const days = geteachDayOfInterval({ start: startOfMonth, end: endOfMonth });
+  const startOfMonth = getStartOfMonth(date)
+  const endOfMonth = getEndOfMonth(date)
+  const days = geteachDayOfInterval({ start: startOfMonth, end: endOfMonth })
 
   return (
-    <Grid templateColumns="repeat(7, 1fr)">
+    <Grid
+      templateColumns="repeat(7, 1fr)"
+      height="100%"
+      borderTop="1px solid"
+      borderLeft="1px solid"
+      borderColor="gray.200"
+    >
       {days.map(day => (
-        <Box key={day} border="1px" borderColor="gray.200" p={3}>
-          {format(day, "d")} 
+        <Box
+          key={day}
+          p={3}
+          borderBottom="1px solid"
+          borderRight="1px solid"
+          borderColor="gray.200"
+          bgColor="white"
+        >
+          {format(day, "d")}
         </Box>
       ))}
     </Grid>
-  );
-};
+  )
+}
 
-export default MonthView;
+export default MonthView
