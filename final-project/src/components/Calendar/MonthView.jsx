05f075ca0932a170d250d3b07aec544519d97b08
Mariela <mariela.spsnv@gmail.com>
2023-08-24 02:32:53 +0300

    display week day at the top of month view
    Signed-off-by: Mariela <mariela.spsnv@gmail.com>

--- final-project/src/components/Calendar/MonthView.jsx
+++ final-project/src/components/Calendar/MonthView.jsx
@@ -6,11 +6,21 @@ import {
 } from "../../services/calendar.services"
 import format from "date-fns/format"
 import { COOL_PURPLE } from "../../common/colors"
+import isToday from "date-fns/isToday"
+import startOfWeek from "date-fns/startOfWeek"
+import endOfWeek from 'date-fns/endOfWeek';
+
 
 const MonthView = ({ date }) => {
   const startOfMonth = getStartOfMonth(date)
   const endOfMonth = getEndOfMonth(date)
-  const days = geteachDayOfInterval({ start: startOfMonth, end: endOfMonth })
+
+  const startOfCalendarView = startOfWeek(startOfMonth, { weekStartsOn: 0 });
+  const endOfCalendarView = endOfWeek(endOfMonth, { weekStartsOn: 0 });
+
+  const days = geteachDayOfInterval({ start: startOfCalendarView, end: endOfCalendarView });
+
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
   return (
     <Grid
@@ -20,20 +30,41 @@ const MonthView = ({ date }) => {
       borderLeft="1px solid"
       borderColor="gray.200"
     >
-      {days.map(day => (
+      {weekdays.map(weekday => (
         <Box
-          key={day}
-          p={3}
+          key={weekday}
+          p={1}
           borderBottom="1px solid"
           borderRight="1px solid"
           borderColor="gray.200"
-          bgColor="white"
+          fontWeight="bold"
+          bgColor="gray.100"
+          textAlign="center"
         >
-          {format(day, "d")}
+          {weekday}
         </Box>
       ))}
+      {days.map(day => {
+        const isCurrentDay = isToday(day);
+        const isOutsideCurrentMonth = day.getMonth() !== date.getMonth();
+        
+        return (
+          <Box
+            key={day}
+            p={3}
+            borderBottom="1px solid"
+            borderRight="1px solid"
+            borderColor="gray.200"
+            bgColor={isCurrentDay ? COOL_PURPLE : isOutsideCurrentMonth ? 'gray.100' : "white"}
+            color={isOutsideCurrentMonth ? 'gray.400' : 'black'}
+            fontWeight={isCurrentDay ? 'bold' : 'normal'}
+          >
+            {format(day, "d")}
+          </Box>
+        )
+      })}
     </Grid>
-  )
+  );
 }
 
 export default MonthView
