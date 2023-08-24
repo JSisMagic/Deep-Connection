cefbc4bf9ca37ee4bad38387f72c64a4178e77b5
gerganadq <gerganaquievy@gmail.com>
2023-08-24 19:26:37 +0300

    UI Improvement: DayView

--- final-project/src/components/Calendar/DayView.jsx
+++ final-project/src/components/Calendar/DayView.jsx
@@ -1,32 +1,29 @@
-import { Box, Text } from "@chakra-ui/react";
-
+import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react"
+import EventsColumn from "./EventsColumn"
+import HoursColumn from "./HoursColumn"
 
 const DayView = ({ date, events = [] }) => {
-  const hours = Array.from({ length: 24 }).map((_, i) => i);
-
-  const renderEventsForHour = (hour) => {
+  const renderEventsForHour = hour => {
     return events
       .filter(event => {
-        const eventStartHour = new Date(event.startDate).getHours();
-        return eventStartHour === hour;
+        const eventStartHour = new Date(event.startDate).getHours()
+        return eventStartHour === hour
       })
       .map((event, index) => (
         <Text key={index} mt={2} fontSize="sm" color="blue.600">
-          {event.title} ({new Date(event.startDate).getHours()}:{new Date(event.startDate).getMinutes()} - {new Date(event.endDate).getHours()}:{new Date(event.endDate).getMinutes()})
+          {event.title} ({new Date(event.startDate).getHours()}:
+          {new Date(event.startDate).getMinutes()} - {new Date(event.endDate).getHours()}:
+          {new Date(event.endDate).getMinutes()})
         </Text>
-      ));
-  };
+      ))
+  }
 
   return (
-    <Box>
-      {hours.map(hour => (
-        <Box key={hour} border="1px" borderColor="gray.200" height="60px" padding={2}>
-          <Text>{hour}:00</Text>
-          {renderEventsForHour(hour)}
-        </Box>
-      ))}
-    </Box>
-  );
-};
+    <Flex>
+      <HoursColumn />
+      <EventsColumn />
+    </Flex>
+  )
+}
 
-export default DayView;
+export default DayView
