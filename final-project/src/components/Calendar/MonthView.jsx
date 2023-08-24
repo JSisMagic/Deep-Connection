477b55eede3a7d1b36facc320b27c142c88be248
gerganadq <gerganaquievy@gmail.com>
2023-08-24 19:27:13 +0300

    UI Improvement: MonthView

--- final-project/src/components/Calendar/MonthView.jsx
+++ final-project/src/components/Calendar/MonthView.jsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from "@chakra-ui/react"
+import { Grid, Box, Stack } from "@chakra-ui/react"
 import {
   getStartOfMonth,
   getEndOfMonth,
@@ -8,63 +8,67 @@ import format from "date-fns/format"
 import { COOL_PURPLE } from "../../common/colors"
 import isToday from "date-fns/isToday"
 import startOfWeek from "date-fns/startOfWeek"
-import endOfWeek from 'date-fns/endOfWeek';
-
+import endOfWeek from "date-fns/endOfWeek"
 
 const MonthView = ({ date }) => {
   const startOfMonth = getStartOfMonth(date)
   const endOfMonth = getEndOfMonth(date)
 
-  const startOfCalendarView = startOfWeek(startOfMonth, { weekStartsOn: 0 });
-  const endOfCalendarView = endOfWeek(endOfMonth, { weekStartsOn: 0 });
+  const startOfCalendarView = startOfWeek(startOfMonth, { weekStartsOn: 0 })
+  const endOfCalendarView = endOfWeek(endOfMonth, { weekStartsOn: 0 })
 
-  const days = geteachDayOfInterval({ start: startOfCalendarView, end: endOfCalendarView });
+  const days = geteachDayOfInterval({ start: startOfCalendarView, end: endOfCalendarView })
 
-  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
   return (
-    <Grid
-      templateColumns="repeat(7, 1fr)"
-      height="100%"
-      borderTop="1px solid"
-      borderLeft="1px solid"
-      borderColor="gray.200"
-    >
-      {weekdays.map(weekday => (
-        <Box
-          key={weekday}
-          p={1}
-          borderBottom="1px solid"
-          borderRight="1px solid"
-          borderColor="gray.200"
-          fontWeight="bold"
-          bgColor="gray.100"
-          textAlign="center"
-        >
-          {weekday}
-        </Box>
-      ))}
-      {days.map(day => {
-        const isCurrentDay = isToday(day);
-        const isOutsideCurrentMonth = day.getMonth() !== date.getMonth();
-        
-        return (
+    <Box height="100%" >
+      <Grid templateColumns="repeat(7, 1fr)" position="sticky" top={16} >
+        {weekdays.map(weekday => (
           <Box
-            key={day}
-            p={3}
+            key={weekday}
+            p={1}
             borderBottom="1px solid"
             borderRight="1px solid"
             borderColor="gray.200"
-            bgColor={isCurrentDay ? COOL_PURPLE : isOutsideCurrentMonth ? 'gray.100' : "white"}
-            color={isOutsideCurrentMonth ? 'gray.400' : 'black'}
-            fontWeight={isCurrentDay ? 'bold' : 'normal'}
+            fontWeight="bold"
+            bgColor="gray.100"
+            textAlign="center"
           >
-            {format(day, "d")}
+            {weekday}
           </Box>
-        )
-      })}
-    </Grid>
-  );
+        ))}
+      </Grid>
+      <Grid
+        templateColumns="repeat(7, 1fr)"
+        templateRows="50px, auto"
+        height="100%"
+        borderTop="1px solid"
+        borderLeft="1px solid"
+        borderColor="gray.200"
+      >
+        {days.map(day => {
+          const isCurrentDay = isToday(day)
+          const isOutsideCurrentMonth = day.getMonth() !== date.getMonth()
+
+          return (
+            <Box
+              key={day}
+              p={3}
+              borderBottom="1px solid"
+              borderRight="1px solid"
+              borderColor="gray.200"
+              bgColor={isCurrentDay ? COOL_PURPLE : isOutsideCurrentMonth ? "gray.50" : "white"}
+              color={isOutsideCurrentMonth ? "gray.400" : "black"}
+              fontWeight={isCurrentDay ? "bold" : "normal"}
+            >
+              {format(day, "d")}
+            </Box>
+          )
+        })}
+      </Grid>
+    </Box>
+  )
 }
 
 export default MonthView
