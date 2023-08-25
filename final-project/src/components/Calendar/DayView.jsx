1beff323bb79d617c8c232c7aa19e71e55f69cad
gerganadq <gerganaquievy@gmail.com>
2023-08-25 19:51:51 +0300

    Optimize: Fetch events for the entire day

--- final-project/src/components/Calendar/DayView.jsx
+++ final-project/src/components/Calendar/DayView.jsx
@@ -1,41 +1,23 @@
-import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import EventsColumn from "./EventsColumn"
+import HoursColumn from "./HoursColumn"
+import { fetchEventsForInterval } from "../../services/event.services"
+import { addDays } from "date-fns"
 
-const DayView = ({ events = [] }) => {
-  const hours = Array.from({ length: 24 }).map((_, i) => i);
+const DayView = ({ date }) => {
+  const [events, setEvents] = useState([])
 
-  const renderEventsForHour = (hour) => {
-    return events
-      .filter((event) => {
-        const eventStartHour = new Date(event.startDate).getHours();
-        return eventStartHour === hour;
-      })
-      .map((event, index) => (
-        <Text key={index} mt={2} fontSize="sm" color="blue.600">
-          {event.title} ({new Date(event.startDate).getHours()}:
-          {new Date(event.startDate).getMinutes()} -{" "}
-          {new Date(event.endDate).getHours()}:
-          {new Date(event.endDate).getMinutes()})
-        </Text>
-      ));
-  };
+  useEffect(() => {
+    fetchEventsForInterval(new Date(date), addDays(date, 1)).then(setEvents)
+  }, [date])
 
   return (
-    <Box>
-      {hours.map((hour) => (
-        <Box
-          key={hour}
-          border="1px"
-          borderColor="gray.200"
-          height="60px"
-          padding={2}
-        >
-          <Text>{hour}:00</Text>
-          {renderEventsForHour(hour)}
-        </Box>
-      ))}
-    </Box>
-  );
-};
+    <Flex>
+      <HoursColumn />
+      <EventsColumn date={new Date(date)} borderLeft={true} events={events} />
+    </Flex>
+  )
+}
 
-export default DayView;
+export default DayView
