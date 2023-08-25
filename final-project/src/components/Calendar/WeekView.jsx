0b1349cc97b95648784c542b5b4e7c5e8ec49532
gerganadq <gerganaquievy@gmail.com>
2023-08-25 23:37:20 +0300

    Refactor WeekView for Workweek Option

--- final-project/src/components/Calendar/WeekView.jsx
+++ final-project/src/components/Calendar/WeekView.jsx
@@ -1,48 +1,67 @@
 /* eslint-disable react/prop-types */
-import { Box, Button, Center, Flex, Grid, GridItem, Heading, IconButton } from "@chakra-ui/react"
-import format from "date-fns/format"
-import { useEffect, useState } from "react"
-import { addDaysToDate, getStartOfWeek } from "../../services/calendar.services"
-import { fetchEventsForInterval } from "../../services/event.services"
-import EventsColumn from "./EventsColumn"
-import HoursColumn from "./HoursColumn"
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons"
-import { addWeeks, subWeeks } from "date-fns"
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  IconButton,
+} from "@chakra-ui/react";
+import format from "date-fns/format";
+import { useEffect, useState } from "react";
+import {
+  addDaysToDate,
+  getStartOfWeek,
+} from "../../services/calendar.services";
+import { fetchEventsForInterval } from "../../services/event.services";
+import EventsColumn from "./EventsColumn";
+import HoursColumn from "./HoursColumn";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import { addWeeks, subWeeks } from "date-fns";
 
 const WeekView = ({ date, setDate, isWorkWeek = false }) => {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+
+  const startDay = isWorkWeek ? 1 : 0; // 1 for Monday, 0 for Sunday
+  const endDay = isWorkWeek ? 5 : 6;
+  const daysCount = endDay - startDay + 1;
+
   useEffect(() => {
     if (date) {
-      const startOfWeek = getStartOfWeek(date)
-      const endOfWeek = addDaysToDate(startOfWeek, 6)
+      const startOfWeek = getStartOfWeek(date);
+      const endOfWeek = addDaysToDate(startOfWeek, 6);
 
-      fetchEvents(startOfWeek, endOfWeek)
+      fetchEvents(startOfWeek, endOfWeek);
     }
-  }, [date])
+  }, [date]);
 
   const fetchEvents = async (startOfWeek, endOfWeek) => {
-    const fetchedEvents = await fetchEventsForInterval(startOfWeek, endOfWeek)
-    setEvents(fetchedEvents)
-  }
+    const fetchedEvents = await fetchEventsForInterval(startOfWeek, endOfWeek);
+    setEvents(fetchedEvents);
+  };
 
   // TODO conditional rendering if isWorkWeek === true to render 5 days (monday - friday)
-  const days = Array.from({ length: 7 }).map((_, i) => addDaysToDate(getStartOfWeek(date), i))
+  const days = Array.from({ length: daysCount }).map((_, i) =>
+    addDaysToDate(getStartOfWeek(date), startDay + i)
+  );
 
-  const handleNavigate = action => {
+  const handleNavigate = (action) => {
     return () => {
       switch (action) {
         case "today":
-          setDate(new Date())
-          break
+          setDate(new Date());
+          break;
         case "prev":
-          setDate(prev => subWeeks(prev, 1))
-          break
+          setDate((prev) => subWeeks(prev, 1));
+          break;
         case "next":
-          setDate(prev => addWeeks(prev, 1))
+          setDate((prev) => addWeeks(prev, 1));
       }
-    }
-  }
-  console.log(days)
+    };
+  };
+  console.log(days);
   return (
     <Box>
       <Grid templateColumns="repeat(3, 1fr)" py={2}>
@@ -51,12 +70,20 @@ const WeekView = ({ date, setDate, isWorkWeek = false }) => {
         </Button>
 
         <Flex justify="center" align="center" gap={3}>
-          <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
+          <IconButton
+            size="sm"
+            icon={<ArrowBackIcon />}
+            onClick={handleNavigate("prev")}
+          />
           <Heading w="300px" size="md" textAlign="center">
             {/* {format(date, `${days[0].getDate()}-${days[6].getDate()}, MMMM`)} */}
-            {format(days[0], "PP")} - {format(days[6], "PP")}
+            {format(days[0], "PP")} - {format(days[days.length - 1], "PP")}
           </Heading>
-          <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
+          <IconButton
+            size="sm"
+            icon={<ArrowForwardIcon />}
+            onClick={handleNavigate("next")}
+          />
         </Flex>
       </Grid>
       <Grid
@@ -71,7 +98,11 @@ const WeekView = ({ date, setDate, isWorkWeek = false }) => {
         </GridItem>
         {days.map((day, i) => (
           <GridItem key={day} colStart={i + 2}>
-            <Grid templateRows="40px 1fr" borderLeft="1px solid" borderColor="gray.300">
+            <Grid
+              templateRows="40px 1fr"
+              borderLeft="1px solid"
+              borderColor="gray.300"
+            >
               <GridItem rowEnd={2} bgColor="gray.50">
                 <Center>{format(day, "EEEE, MMMM d")}</Center>
               </GridItem>
@@ -83,7 +114,7 @@ const WeekView = ({ date, setDate, isWorkWeek = false }) => {
         ))}
       </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default WeekView
+export default WeekView;
