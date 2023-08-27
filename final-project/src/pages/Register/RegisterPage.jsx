b7281afaa33a36ba11916197ba0423539b1d7e8c
gerganadq <gerganaquievy@gmail.com>
2023-08-27 23:19:40 +0300

    Refactor Registration Form validations: Improved form validation, added password visibility toggle, and updated error handling in the RegisterPage component.

--- final-project/src/pages/Register/RegisterPage.jsx
+++ final-project/src/pages/Register/RegisterPage.jsx
@@ -1,26 +1,15 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import bgImage from "../../assets/images/hero.png";
-import { errorMessages } from "../../common/error-messages";
-import { registerUser } from "../../services/auth.services";
-import { createUser, getUser } from "../../services/users.services";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { useForm } from "react-hook-form"; // Add this import
+import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, InputGroup, InputRightElement, Stack, Text } from '@chakra-ui/react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import bgImage from '../../assets/images/hero.png';
+import { errorMessages } from '../../common/error-messages';
+import { registerUser } from '../../services/auth.services';
+import { createUser, getUser } from '../../services/users.services';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { useForm } from 'react-hook-form'; // Add this import
+import validation from '../../common/validation-enums';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 const RegisterPage = () => {
   const {
@@ -28,12 +17,12 @@ const RegisterPage = () => {
     handleSubmit,
     setError,
     formState: { errors },
-    watch,
+    // watch,
   } = useForm();
   const { setAuthState } = useContext(AuthContext);
   const navigate = useNavigate();
-
   const [showPassword, setShowPassword] = useState(false);
+  const { MIN_USERNAME_LENGTH, MAX_USERNAME_LENGTH, MIN_FIRSTNAME_LENGTH, MAX_FIRSTNAME_LENGTH, MIN_LASTNAME_LENGTH, MAX_LASTNAME_LENGTH, PHONE_NUM_LENGTH, MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH } = validation;
 
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
@@ -44,7 +33,7 @@ const RegisterPage = () => {
 
     const user = await getUser(username);
     if (user !== null) {
-      return setError("username", { message: errorMessages.USER_EXISTS });
+      return setError('username', { message: errorMessages.USER_EXISTS });
     }
 
     try {
@@ -61,70 +50,131 @@ const RegisterPage = () => {
       });
 
       setAuthState((prev) => ({ ...prev, userData: userData }));
-      console.log("successful registration");
-      navigate("/");
+      console.log('successful registration');
+      navigate('/');
     } catch (e) {
-      setError("email", { message: errorMessages.EMAIL_EXISTS });
+      setError('email', { message: errorMessages.EMAIL_EXISTS });
     }
   };
-
+  console.log('errors', errors);
   return (
-    <Flex
-      direction="column"
-      height="100%"
-      width="100%"
-      justify="center"
-      align="center"
-      bgImage={bgImage}
-    >
-      <Box
-        width="30%"
-        bgColor="rgba(255,255,255)"
-        padding="2rem"
-        borderRadius="lg"
-        boxShadow="2xl"
-      >
+    <Flex direction="column" height="100%" width="100%" justify="center" align="center" bgImage={bgImage}>
+      <Box width="30%" bgColor="rgba(255,255,255)" padding="2rem" borderRadius="lg" boxShadow="2xl">
         <Heading textAlign="center">Register</Heading>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Stack mt={10}>
-            <FormControl isInvalid={errors.email}>
+            <FormControl id="email" isRequired isInvalid={errors.email}>
               <FormLabel>Email</FormLabel>
-              <Input type="email" {...register("email")} />
+              <Input
+                id="email"
+                {...register('email', {
+                  required: 'This is required!',
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: 'Entered value does not match email format!',
+                  },
+                })}
+              />
               <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl isInvalid={errors.username}>
+            <FormControl isRequired isInvalid={errors.username}>
               <FormLabel>Username</FormLabel>
-              <Input type="text" {...register("username")} />
+              <Input
+                type="text"
+                {...register('username', {
+                  required: 'This is required!',
+                  minLength: {
+                    value: MIN_USERNAME_LENGTH,
+                    message: `Minimum length should be ${MIN_USERNAME_LENGTH}`,
+                  },
+                  maxLength: {
+                    value: MAX_USERNAME_LENGTH,
+                    message: `Maximum length should be ${MAX_USERNAME_LENGTH}`,
+                  },
+                })}
+              />
               <FormErrorMessage>{errors?.username?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isRequired isInvalid={errors.firstName}>
               <FormLabel>First name</FormLabel>
-              <Input type="text" {...register("firstName")} />
+              <Input
+                type="text"
+                {...register('firstName', {
+                  required: 'This is required!',
+                  minLength: {
+                    value: MIN_FIRSTNAME_LENGTH,
+                    message: `Minimum length should be ${MIN_FIRSTNAME_LENGTH}`,
+                  },
+                  maxLength: {
+                    value: MAX_FIRSTNAME_LENGTH,
+                    message: `Maximum length should be ${MAX_FIRSTNAME_LENGTH}`,
+                  },
+                  validate: {
+                    containsOnlyLetters: (value) => /^[a-zA-Z]+$/.test(value) || 'First name must contain only uppercase and lowercase letters!',
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors?.firstName?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isRequired isInvalid={errors.lastName}>
               <FormLabel>Last name</FormLabel>
-              <Input type="text" {...register("lastName")} />
+              <Input
+                type="text"
+                {...register('lastName', {
+                  required: 'This is required!',
+                  minLength: {
+                    value: MIN_LASTNAME_LENGTH,
+                    message: `Minimum length should be ${MIN_LASTNAME_LENGTH}`,
+                  },
+                  maxLength: {
+                    value: MAX_LASTNAME_LENGTH,
+                    message: `Maximum length should be ${MAX_LASTNAME_LENGTH}`,
+                  },
+                  validate: {
+                    containsOnlyLetters: (value) => /^.[A-Za-z]+$/.test(value) || 'Last name must contain only uppercase and lowercase letters!',
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors?.lastName?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isRequired isInvalid={errors.phone}>
               <FormLabel>Phone number</FormLabel>
-              <Input type="text" {...register("phone")} />
+              <Input
+                type="text"
+                {...register('phone', {
+                  required: 'This is required!',
+                  validate: {
+                    haveFixedLength: (value) => value.length === PHONE_NUM_LENGTH || 'Phone number must have 10 digit!',
+                    containsOnlyDigits: (value) => /^[0-9]+$/.test(value) || 'Phone number must have only digits!',
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors?.phone?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl isInvalid={errors.password}>
+            <FormControl isRequired isInvalid={errors.password}>
               <FormLabel>Password</FormLabel>
               <InputGroup>
                 <Input
-                  type={showPassword ? "text" : "password"}
-                  {...register("password")}
+                  type={showPassword ? 'text' : 'password'}
+                  {...register('password', {
+                    minLength: {
+                      value: MIN_PASSWORD_LENGTH,
+                      message: `Minimum length should be ${MIN_PASSWORD_LENGTH}`,
+                    },
+                    maxLength: {
+                      value: MAX_PASSWORD_LENGTH,
+                      message: `Maximum length should be ${MAX_PASSWORD_LENGTH}`,
+                    },
+                    required: 'This is required!',
+                    validate: {
+                      containsUpperLetter: (value) => /^.*[a-z]+.*/.test(value) || 'Password must contain at least one lowercase letter!',
+                      containsCapitalLetter: (value) => /^.*[A-Z]+.*/.test(value) || 'Password must contain at least one capital letter!',
+                      containsNumber: (value) => /^.*[0-9]+.*/.test(value) || 'Password must contain at least one number!',
+                      containsSpecialChar: (value) => /^.*[!@#$%^&*]+.*/.test(value) || 'Password must contain at least one special character!',
+                    },
+                  })}
                 />
-                <InputRightElement>
-                  <Button
-                    onClick={handleTogglePassword}
-                    variant="ghost"
-                    aria-label="Toggle password visibility"
-                  >
-                    {showPassword ? "Hide" : "Show"}
-                  </Button>
-                </InputRightElement>
+                <InputRightElement>{showPassword ? <ViewIcon cursor="pointer" onClick={handleTogglePassword} /> : <ViewOffIcon cursor="pointer" onClick={handleTogglePassword} />}</InputRightElement>
               </InputGroup>
               <FormErrorMessage>{errors?.password?.message}</FormErrorMessage>
             </FormControl>
@@ -138,8 +188,8 @@ const RegisterPage = () => {
             </Text>
           )}
           <Text align="center">
-            Already registered? Sign in{" "}
-            <Link to="/login" style={{ color: "purple" }}>
+            Already registered? Sign in{' '}
+            <Link to="/login" style={{ color: 'purple' }}>
               here
             </Link>
           </Text>
