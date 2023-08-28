6185abb9cddbeb850b17dc86f27d71450057b633
Mariela <mariela.spsnv@gmail.com>
2023-08-28 23:02:56 +0300

    add delete users from contact lists
    Signed-off-by: Mariela <mariela.spsnv@gmail.com>

--- final-project/src/components/ContactList/ContactList.jsx
+++ final-project/src/components/ContactList/ContactList.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { auth } from '../../config/firebase';  
 import {
-  Box, Heading, List, ListItem,
-  Input, Button, Stack, VStack,
-  Spinner, Select
+  Box, Heading, List, ListItem, Collapse, Input, Button,
+  Stack, VStack, Spinner, Select, Divider
 } from '@chakra-ui/react';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaChevronDown, FaChevronUp, FaTimes, FaUserPlus, FaPlus  } from 'react-icons/fa';
 import { getUserByEmail, getUserContactLists, createContactListForUser, updateContactListForUser, deleteContactListForUser } from '../../services/users.services';
 
 const ContactList = () => {
@@ -15,6 +14,7 @@ const ContactList = () => {
   const [selectedList, setSelectedList] = useState(null);
   const [userUid, setUserUid] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [expandedList, setExpandedList] = useState(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
@@ -93,6 +93,22 @@ const ContactList = () => {
     }
   };
 
+  const handleRemoveUserFromList = async (listId, userId) => {
+    try {
+      const updatedList = { ...contactLists[listId] };
+      if (updatedList.contacts[userId]) {
+        delete updatedList.contacts[userId];
+        await updateContactListForUser(userUid, listId, updatedList);
+        setContactLists(prevLists => ({
+          ...prevLists,
+          [listId]: updatedList
+        }));
+      }
+    } catch (error) {
+      console.log('Error removing user from list:', error.message);
+    }
+  };
+
   if (isLoading) {
     return <Spinner />;
   }
@@ -101,45 +117,94 @@ const ContactList = () => {
     return <p>User not authenticated!</p>
   }
 
-  return (
-    <VStack spacing={5} width="100%">
+  return  (
+    
+    <Box display="flex" alignItems="center" justifyContent="center" height="100vh">
+    <VStack 
+      spacing={4}
+      padding={5}
+      width="30%"
+      bgColor="rgba(255,255,255)"
+      borderRadius="lg"
+      boxShadow="2xl">
       <Heading>Contact Lists</Heading>
+
       {Object.values(contactLists).map(list => (
         <Box key={list.id} p={4} borderWidth="1px" borderRadius="lg" width="100%">
-          <Heading size="md">
+          <Heading size="md" display="flex" alignItems="center" justifyContent="space-between">
             {list.name}
-            <Button size="sm" leftIcon={<FaTrash />} onClick={() => handleDeleteList(list.id)} colorScheme="blue" ml={4}></Button>
+            <Box>
+              <Button 
+                size="sm" 
+                onClick={() => setExpandedList(prev => prev === list.id ? null : list.id)} 
+                mr={2}>
+                  {expandedList === list.id ? <FaChevronUp /> : <FaChevronDown />}
+              </Button>
+              <Button 
+                size="sm" 
+                leftIcon={<FaTrash />} 
+                onClick={() => handleDeleteList(list.id)} 
+                bgColor="#E9D8FD">
+              </Button>
+            </Box>
           </Heading>
-          <List spacing={2}>
-            {Object.values((list.contacts || {})).map(contact => (
-              <ListItem key={contact.id}>{contact.name}</ListItem>
-            ))}
-          </List>
+          <Collapse in={expandedList === list.id}>
+            <List spacing={2}>
+              {Object.values((list.contacts || {})).map(contact => (
+                <ListItem key={contact.id} display="flex" justifyContent="space-between" alignItems="center">
+                  {contact.name}
+                  <Button 
+                    size="sm" 
+                    leftIcon={<FaTimes />} 
+                    onClick={() => handleRemoveUserFromList(list.id, contact.id)} 
+                    variant="outline">
+                     
+                  </Button>
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
         </Box>
       ))}
-      <Stack direction="row" spacing={4} width="100%">
-        <Input
-          value={listName}
-          onChange={e => setListName(e.target.value)}
-          placeholder="Enter list name"
-        />
-        <Button onClick={handleCreateList} colorScheme="blue">Create New List</Button>
-      </Stack>
-      <Stack direction="row" spacing={4} width="100%">
-        <Input
-          value={emailToAdd}
-          onChange={e => setEmailToAdd(e.target.value)}
-          placeholder="Enter user email to add"
-        />
-        <Select placeholder="Select a contact list" onChange={e => setSelectedList(e.target.value)}>
-          {Object.values(contactLists).map(list => (
-            <option key={list.id} value={list.id}>{list.name}</option>
-          ))}
-        </Select>
-        <Button onClick={handleAddUserToList} colorScheme="blue">Add User</Button>
-      </Stack>
+
+      <Divider my={4} />
+
+      <Box p={6} width="100%" borderWidth="1px" borderRadius="lg" boxShadow="sm">
+        <Heading size="sm" mb={4}>Manage Contact Lists</Heading>
+        <Stack direction="row" spacing={4} width="100%" mb={4}>
+          <Input
+            value={listName}
+            onChange={e => setListName(e.target.value)}
+            placeholder="Enter list name"
+          />
+          <Button 
+            onClick={handleCreateList} 
+            bgColor="#E9D8FD" 
+            rightIcon={<FaPlus />}>
+          </Button>
+        </Stack>
+        <Stack direction="row" spacing={4} width="100%">
+          <Input
+            value={emailToAdd}
+            onChange={e => setEmailToAdd(e.target.value)}
+            placeholder="Enter user email to add"
+          />
+          <Select placeholder="Select a contact list" onChange={e => setSelectedList(e.target.value)}>
+            {Object.values(contactLists).map(list => (
+              <option key={list.id} value={list.id}>{list.name}</option>
+            ))}
+          </Select>
+          <Button 
+            onClick={handleAddUserToList} 
+            bgColor="#E9D8FD" 
+            rightIcon={<FaUserPlus />}>
+          </Button>
+        </Stack>
+      </Box>
     </VStack>
+  </Box>
   );
+
 }
 
 export default ContactList;
