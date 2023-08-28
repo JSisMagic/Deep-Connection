ac366f0a717c9acced58d7631c06b139d8ef7279
gerganadq <gerganaquievy@gmail.com>
2023-08-28 18:42:10 +0300

    Revert "Fix bug in users.services.js"
    This reverts commit b8741eb42d7cc6e3ddf4dcb55eeab08437c11462.

--- final-project/src/services/users.services.js
+++ final-project/src/services/users.services.js
@@ -1,4 +1,4 @@
-import { equalTo, set, get, orderByChild, query, ref, update, remove } from "firebase/database"
+import { equalTo, set, get, orderByChild, query, ref, update } from "firebase/database"
 import { db } from "../config/firebase"
 
 export const getUser = async username => {
@@ -11,7 +11,7 @@ export const createUser = async (data = {}) => {
     throw new Error("Data should be a valid object!")
   }
 
-  const userExists = await getUser(data.uid)
+  const userExists = await getUser(data.uid)  
 
   if (userExists !== null) {
     throw new Error(`User with UID ${data.uid} already exists!`)
@@ -19,8 +19,7 @@ export const createUser = async (data = {}) => {
 
   await set(ref(db, `users/${data.uid}`), {
     ...data,
-    contactLists: {},
-    memberLists: {} // Add memberLists
+    contactLists: {}
   });
 
   return { ...data }
@@ -55,65 +54,21 @@ export const updateUser = async (uid, data) => {
 export const getUserContactLists = async (uid) => {
   const contactListsRef = ref(db, `users/${uid}/contactLists`);
   const snapshot = await get(contactListsRef);
-  const result = {};
-
-  Object.values(snapshot.val() || {}).map(value => {
-    result[value.id] = {
-      ...value,
-      contacts: value.contacts !== undefined ? value.contacts : {}
-    }
-  })
-
-  return result;
+  return snapshot.val() || {};
 }
 
 export const createContactListForUser = async (uid, contactList) => {
   const id = new Date().getTime().toString();
+
   const listWithContacts = { ...contactList, contacts: contactList.contacts || [] };
 
   await set(ref(db, `users/${uid}/contactLists/${id}`), listWithContacts);
   return { id, ...listWithContacts };
 }
 
-export const updateContactListForUser = async (uid, listId, updatedContactList) => {
-  await update(ref(db, `users/${uid}/contactLists/${listId}`), updatedContactList)
+export const updateContactListForUser = async (username, listId, updatedContactList) => {
+  await update(ref(db, `users/${username}/contactLists/${listId}`), updatedContactList)
   return { ...updatedContactList }
-}
-
-export const deleteContactListForUser = async (uid, listId) => {
-  await remove(ref(db, `users/${uid}/contactLists/${listId}`));
-}
-
-export const getUserMemberLists = async (uid) => {
-  const memberListsRef = ref(db, `users/${uid}/memberLists`);
-  const snapshot = await get(memberListsRef);
-  const result = {};
-
-  Object.values(snapshot.val() || {}).map(value => {
-    result[value.id] = {
-      ...value,
-      members: value.members !== undefined ? value.members : {}
-    }
-  })
-
-  return result;
-}
-
-export const createMemberListForUser = async (uid, memberList) => {
-  const id = new Date().getTime().toString();
-  const listWithMembers = { ...memberList, members: memberList.members || [] };
-
-  await set(ref(db, `users/${uid}/memberLists/${id}`), listWithMembers);
-  return { id, ...listWithMembers };
-}
-
-export const updateMemberListForUser = async (uid, listId, updatedMemberList) => {
-  await update(ref(db, `users/${uid}/memberLists/${listId}`), updatedMemberList)
-  return { ...updatedMemberList }
-}
-
-export const deleteMemberListForUser = async (uid, listId) => {
-  await remove(ref(db, `users/${uid}/memberLists/${listId}`));
 }
 
 export const getUserByEmail = async email => {
