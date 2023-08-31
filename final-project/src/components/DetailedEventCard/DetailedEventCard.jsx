2dae30cc4dccd9554369d0d2e5c2afbdd20cdcb7
gerganadq <gerganaquievy@gmail.com>
2023-08-31 20:52:41 +0300

    Optimize `DetailedEventCard` Component Structure Integrate Event Image & Refactor Description Handling

--- final-project/src/components/DetailedEventCard/DetailedEventCard.jsx
+++ final-project/src/components/DetailedEventCard/DetailedEventCard.jsx
@@ -1,39 +1,32 @@
-import {
-  Box,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  Text,
-  Heading,
-  Stack,
-} from "@chakra-ui/react"
+import { Box, Flex, Icon, Text, Heading, Stack, Image } from "@chakra-ui/react"
 import { format } from "date-fns"
+import { FaLocationDot } from "react-icons/fa6"
 
 const DetailedEventCard = ({ detailedEventData }) => {
   return (
-    <Stack gap={3}>
+    <Stack gap={3} py={5}>
       <Heading size="lg">{detailedEventData.title}</Heading>
-      <Box p={5} bg="gray.100" borderRadius="lg">
-        <Heading size="md" mb={1}>
-          {format(detailedEventData.startDate, "MMMM d")}
-        </Heading>
-        <Text fontWeight={700}>@
-          {format(detailedEventData.startDate, "HH:mm")} -{" "}
-          {format(detailedEventData.endDate, "HH:mm O")}
-        </Text>
-      </Box>
-      <Text fontSize={18} fontWeight={500}>
-        {detailedEventData.description}
-      </Text>
-      <Box>
-        <Text fontWeight="bold">Location:</Text>
-        <Heading fontSize={13} fontWeight={600} maxW="150px" mb={1}>
-          {detailedEventData.location}
-        </Heading>
-      </Box>
+      <Flex p={5} bg="gray.100" borderRadius="lg" gap={3} justifyContent="space-between" align="center">
+        <Box flexGrow={1} minWidth="60%">
+          <Heading size="md" mb={1}>
+            {format(detailedEventData.startDate, "MMMM d")}
+          </Heading>
+          <Text fontWeight={700}>
+            @{format(detailedEventData.startDate, "HH:mm")} -{" "}
+            {format(detailedEventData.endDate, "HH:mm O")}
+          </Text>
+        </Box>
+        {detailedEventData?.location && (
+          <Flex gap={1}>
+            <Icon as={FaLocationDot} boxSize={6} color="red.700"/>
+            <Text fontSize={14} fontWeight={600}>
+              {detailedEventData.location}
+            </Text>
+          </Flex>
+        )}
+      </Flex>
+      <Image src={detailedEventData.image} borderRadius="lg" />
+      <Box dangerouslySetInnerHTML={{ __html: detailedEventData.description }} />
     </Stack>
   )
 }
