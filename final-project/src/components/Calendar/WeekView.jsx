c4f3003d5c077cbce18b8d185d8ee3e0c1101a4c
gerganadq <gerganaquievy@gmail.com>
2023-08-31 20:41:36 +0300

    Enhance: WeekView UI, Navigation &  Date Display

--- final-project/src/components/Calendar/WeekView.jsx
+++ final-project/src/components/Calendar/WeekView.jsx
@@ -1,65 +1,48 @@
 /* eslint-disable react/prop-types */
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  IconButton,
-} from "@chakra-ui/react";
-import { addWeeks, subWeeks } from "date-fns";
-import format from "date-fns/format";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import {
-  addDaysToDate,
-  getStartOfWeek,
-} from "../../services/calendar.services";
-import { getEventsForUser } from "../../services/event.services";
-import EventsColumn from "./EventsColumn";
-import HoursColumn from "./HoursColumn";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons"
+import { Box, Button, Center, Flex, Grid, GridItem, Heading, IconButton } from "@chakra-ui/react"
+import { addWeeks, subWeeks } from "date-fns"
+import format from "date-fns/format"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../../context/AuthContext"
+import { addDaysToDate, getStartOfWeek } from "../../services/calendar.services"
+import { getEventsForUser } from "../../services/event.services"
+import EventsColumn from "./EventsColumn"
+import HoursColumn from "./HoursColumn"
 
-const WeekView = ({
-  date,
-  setDate,
-  isWorkWeek = false,
-  onOpenDetailedModal,
-}) => {
-  const { userData } = useContext(AuthContext);
-  const [events, setEvents] = useState([]);
+const WeekView = ({ date, setDate, isWorkWeek = false, onOpenDetailedModal }) => {
+  const { userData } = useContext(AuthContext)
+  const [events, setEvents] = useState([])
 
-  const startDay = isWorkWeek ? 1 : 0; // 1 for Monday, 0 for Sunday
-  const endDay = isWorkWeek ? 5 : 6;
-  const daysCount = endDay - startDay + 1;
+  const startDay = isWorkWeek ? 1 : 0 // 1 for Monday, 0 for Sunday
+  const endDay = isWorkWeek ? 5 : 6
+  const daysCount = endDay - startDay + 1
 
   useEffect(() => {
     if (date && userData?.uid) {
-      getEventsForUser(userData?.uid).then(setEvents);
+      getEventsForUser(userData?.uid).then(setEvents)
     }
-  }, [date, userData]);
+  }, [date, userData])
 
   // TODO conditional rendering if isWorkWeek === true to render 5 days (monday - friday)
   const days = Array.from({ length: daysCount }).map((_, i) =>
     addDaysToDate(getStartOfWeek(date), startDay + i)
-  );
+  )
 
-  const handleNavigate = (action) => {
+  const handleNavigate = action => {
     return () => {
       switch (action) {
         case "today":
-          setDate(new Date());
-          break;
+          setDate(new Date())
+          break
         case "prev":
-          setDate((prev) => subWeeks(prev, 1));
-          break;
+          setDate(prev => subWeeks(prev, 1))
+          break
         case "next":
-          setDate((prev) => addWeeks(prev, 1));
+          setDate(prev => addWeeks(prev, 1))
       }
-    };
-  };
+    }
+  }
 
   return (
     <Box>
@@ -69,23 +52,17 @@ const WeekView = ({
         </Button>
 
         <Flex justify="center" align="center" gap={3}>
-          <IconButton
-            size="sm"
-            icon={<ArrowBackIcon />}
-            onClick={handleNavigate("prev")}
-          />
+          <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
           <Heading w="300px" size="md" textAlign="center">
             {/* {format(date, `${days[0].getDate()}-${days[6].getDate()}, MMMM`)} */}
             {format(days[0], "PP")} - {format(days[days.length - 1], "PP")}
           </Heading>
-          <IconButton
-            size="sm"
-            icon={<ArrowForwardIcon />}
-            onClick={handleNavigate("next")}
-          />
+          <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
         </Flex>
       </Grid>
       <Grid
+        height="700px"
+        overflowY="auto"
         templateRows="40px 1fr"
         templateColumns={`50px repeat(${daysCount}, 1fr)`}
         borderLeft="1px solid"
@@ -98,11 +75,7 @@ const WeekView = ({
         </GridItem>
         {days.map((day, i) => (
           <GridItem key={day} colStart={i + 2}>
-            <Grid
-              templateRows="40px 1fr"
-              borderLeft="1px solid"
-              borderColor="gray.300"
-            >
+            <Grid templateRows="40px 1fr" borderLeft="1px solid" borderColor="gray.300">
               <GridItem rowEnd={2} bgColor="gray.50">
                 <Center>{format(day, "E, do")}</Center>
               </GridItem>
@@ -119,7 +92,7 @@ const WeekView = ({
         ))}
       </Grid>
     </Box>
-  );
-};
+  )
+}
 
-export default WeekView;
+export default WeekView
