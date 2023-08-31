43d97b6567149ac5fc93e431ee9478740c5c2f7f
gerganadq <gerganaquievy@gmail.com>
2023-08-31 20:34:58 +0300

    Update: EventBox component to handle differ views

--- final-project/src/components/Calendar/EventBox.jsx
+++ final-project/src/components/Calendar/EventBox.jsx
@@ -1,40 +1,29 @@
-import { useState } from "react"
-import {
-  Box,
-  Heading,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  Text,
-} from "@chakra-ui/react"
-import { categoryColors } from "../../common/colors"
+import { Box, Heading, Flex } from "@chakra-ui/react"
 import { format } from "date-fns"
+import { categoryColors } from "../../common/colors"
 
-const EventBox = ({ eventData, isUsedInWeek, onOpenDetailedModal }) => {
+const EventBox = ({ eventData, isUsedInWeek, isUsedInMonth, onOpenDetailedModal }) => {
   return (
     <Box
       onClick={() => onOpenDetailedModal(eventData)}
       border="1px grey"
       bg="blue.100"
       w={isUsedInWeek ? "100%" : "full"}
-      height="full"
+      height={isUsedInMonth ? "30px" : "full"}
       overflowWrap="break-word"
       color={`rgb(${categoryColors[eventData.color]})`}
       backgroundColor={`rgba(${categoryColors[eventData.color]}, .2)`}
       padding={2}
       cursor="pointer"
     >
-      <Box>
+      <Flex direction={isUsedInMonth ? "row" : "column"} gap={isUsedInMonth && 1}>
         <Heading fontSize={12} fontWeight={700} maxW="150px" mb={1}>
           {format(eventData.startDate, "H:mm")}
         </Heading>
         <Heading noOfLines={3} size={{ base: "xs" }} fontWeight={600} maxW="170px">
           {eventData.title}
         </Heading>
-      </Box>
+      </Flex>
     </Box>
   )
 }
