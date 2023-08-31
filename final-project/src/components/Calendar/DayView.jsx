22bf0781375be66f49056959d7a4a3d9f44949a3
gerganadq <gerganaquievy@gmail.com>
2023-08-31 20:37:00 +0300

    Update: DayView component for Navigation & ... Date Display

--- final-project/src/components/Calendar/DayView.jsx
+++ final-project/src/components/Calendar/DayView.jsx
@@ -1,11 +1,13 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Box, Grid, Button, IconButton, Heading } from "@chakra-ui/react"
+import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons"
 import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
 import { getEventsForUser } from "../../services/event.services"
 import EventsColumn from "./EventsColumn"
 import HoursColumn from "./HoursColumn"
+import { addDays, format, subDays } from "date-fns"
 
-const DayView = ({ date, onOpenDetailedModal }) => {
+const DayView = ({ date, setDate, onOpenDetailedModal }) => {
   const { user } = useContext(AuthContext)
   const [events, setEvents] = useState([])
 
@@ -15,16 +17,49 @@ const DayView = ({ date, onOpenDetailedModal }) => {
     }
   }, [date, user.uid])
 
+  const handleNavigate = action => {
+    return () => {
+      switch (action) {
+        case "today":
+          setDate(new Date())
+          break
+        case "prev":
+          setDate(prev => subDays(prev, 1))
+          break
+        case "next":
+          setDate(prev => addDays(prev, 1))
+      }
+    }
+  }
+
   return (
-    <Flex>
-      <HoursColumn />
-      <EventsColumn
-        date={new Date(date)}
-        borderLeft={true}
-        events={events}
-        onOpenDetailedModal={onOpenDetailedModal}
-      />
-    </Flex>
+    <Box>
+      <Grid templateColumns="repeat(3, 1fr)" py={2} borderBottom="1px solid" borderColor="gray.200">
+        <Button onClick={handleNavigate("today")} width="max-content">
+          Today
+        </Button>
+
+        <Flex justify="center" align="center" gap={3}>
+          <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
+          <Heading w="300px" size="md" textAlign="center">
+            {/* {format(date, `${days[0].getDate()}-${days[6].getDate()}, MMMM`)} */}
+            {format(date, "PPPP")}
+          </Heading>
+          <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
+        </Flex>
+      </Grid>
+      <Box height="700px" overflowY="scroll">
+        <Flex>
+          <HoursColumn />
+          <EventsColumn
+            date={new Date(date)}
+            borderLeft={true}
+            events={events}
+            onOpenDetailedModal={onOpenDetailedModal}
+          />
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
