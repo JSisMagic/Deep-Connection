f54501a9b1ccd8f80fe07ef8745fef025bb856a0
gerganadq <gerganaquievy@gmail.com>
2023-09-01 20:49:48 +0300

    feat: Create MyEvents Component

--- final-project/src/components/Events/MyEvents.jsx
+++ final-project/src/components/Events/MyEvents.jsx
@@ -1,5 +1,19 @@
+import { useContext, useEffect, useState } from "react"
+import EventsList from "../EventsList/EventsList"
+import { getEventsForUser } from "../../services/event.services"
+import { AuthContext } from "../../context/AuthContext"
+
 const MyEvents = () => {
-  return <div>MyEvents</div>
+  const { user } = useContext(AuthContext)
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    if (user?.uid) {
+      getEventsForUser(user.uid).then(setEvents).catch(console.error)
+    }
+  }, [user?.uid])
+
+  return <EventsList events={events} />
 }
 
 export default MyEvents
