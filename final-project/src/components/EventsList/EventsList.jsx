21f409d352da9672657b24d55ec2697d00039f35
gerganadq <gerganaquievy@gmail.com>
2023-09-03 00:35:28 +0300

    Refactor: EventsList code readability enhancements

--- final-project/src/components/EventsList/EventsList.jsx
+++ final-project/src/components/EventsList/EventsList.jsx
@@ -1,44 +1,50 @@
-import { SearchIcon } from "@chakra-ui/icons"
-import { Input, InputGroup, InputLeftElement, Stack, Heading } from "@chakra-ui/react"
-import EventPageCard from "../EventCard/EventCard"
-import { useEffect, useState } from "react"
-import DetailedEventCard from "../DetailedEventCard/DetailedEventCard"
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+  Heading,
+} from "@chakra-ui/react";
+import EventPageCard from "../EventCard/EventCard";
+import { useEffect, useState } from "react";
+import DetailedEventCard from "../DetailedEventCard/DetailedEventCard";
 
 const EventsList = ({ events = [] }) => {
-  const [filteredEvents, setFilteredEvents] = useState([])
-  const [searchTerm, setSearchTerm] = useState("")
-  const [isOpen, setIsOpen] = useState(false)
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   const [detailedEventData, setDetailedEventData] = useState({
     title: "",
     startDate: new Date(),
     endDate: new Date(),
     description: "",
     location: "",
-  })
+  });
 
-  const onOpen = eventData => {
-    setDetailedEventData({ ...eventData })
-    setIsOpen(true)
-  }
+  const onOpen = (eventData) => {
+    setDetailedEventData({ ...eventData });
+    setIsOpen(true);
+  };
 
   const onClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
       const filtered = events.filter(
-        ev =>
+        (ev) =>
           ev.title?.toLowerCase().includes(searchTerm) ||
           ev.description?.toLowerCase().includes(searchTerm) ||
           ev.location?.toLowerCase().includes(searchTerm)
-      )
+      );
 
-      setFilteredEvents(filtered)
-    }, 300)
+      setFilteredEvents(filtered);
+    }, 300);
 
-    return () => clearTimeout(timer)
-  }, [searchTerm, events])
+    return () => clearTimeout(timer);
+  }, [searchTerm, events]);
 
   return (
     <Stack gap={5}>
@@ -46,7 +52,7 @@ const EventsList = ({ events = [] }) => {
         <Input
           placeholder="Search for events.."
           value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value.toLowerCase())}
+          onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
         />
         <InputLeftElement>
           <SearchIcon />
@@ -54,7 +60,7 @@ const EventsList = ({ events = [] }) => {
       </InputGroup>
       <Stack height="700px" overflowY="auto">
         {filteredEvents.length > 0 ? (
-          filteredEvents.map(event => (
+          filteredEvents.map((event) => (
             <EventPageCard
               key={event.id}
               eventData={event}
@@ -67,9 +73,13 @@ const EventsList = ({ events = [] }) => {
           </Heading>
         )}
       </Stack>
-      <DetailedEventCard isOpen={isOpen} onClose={onClose} detailedEventData={detailedEventData} />
+      <DetailedEventCard
+        isOpen={isOpen}
+        onClose={onClose}
+        detailedEventData={detailedEventData}
+      />
     </Stack>
-  )
-}
+  );
+};
 
-export default EventsList
+export default EventsList;
