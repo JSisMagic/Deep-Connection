8c59e1d3cc15fcb1a248a333547d7f5c0dc2e4b1
gerganadq <gerganaquievy@gmail.com>
2023-09-05 00:52:17 +0300

    Refactor Weather component

--- final-project/src/components/Weather/Weather.jsx
+++ final-project/src/components/Weather/Weather.jsx
@@ -1,12 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Box, Text, Image, Flex, Alert, AlertIcon, AlertDescription } from '@chakra-ui/react';
-import { getWeather } from '../../services/weather.service';
-import { weatherIcon } from '../../common/weather-icons.enum';
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+} from "@chakra-ui/react";
+import { getWeather } from "../../services/weather.service";
+import { weatherIcon } from "../../common/weather-icons.enum";
+
+// Import the icons with unique names
+import { WiStrongWind, WiHumidity, WiCloudy } from "react-icons/wi";
 
 export default function Weather() {
   const [state, setState] = useState({ location: null, weather: null });
-  // const toast = useToast();
-  // const { addToast } = useContext(AppContext);
 
   const setLocation = () => {
     if (!navigator.geolocation) return;
@@ -15,21 +24,18 @@ export default function Weather() {
       (position) => {
         setState((state) => ({
           ...state,
-          location: position.coords.latitude + ',' + position.coords.longitude,
+          location: position.coords.latitude + "," + position.coords.longitude,
         }));
       },
       (error) => {
-        // addToast({
-        //   title: 'Error',
-        //   description: error.message,
-        //   status: 'error',
-        // });
         console.log(error.message);
       }
     );
   };
 
-  useEffect(() => setLocation(), []);
+  useEffect(() => {
+    setLocation();
+  }, []);
 
   useEffect(() => {
     if (state.location === null) return;
@@ -38,11 +44,6 @@ export default function Weather() {
       .then((response) => response.json())
       .then((data) => setState((state) => ({ ...state, weather: data })))
       .catch((e) => {
-        // addToast({
-        //   title: 'Error',
-        //   description: e.message,
-        //   status: 'error',
-        // });
         console.log(e.message);
       });
   }, [state.location]);
@@ -53,60 +54,76 @@ export default function Weather() {
   };
 
   return (
-    <>
-      {/* <Alert status="error">
-        <AlertIcon />
-        <AlertDescription>Couldnt fetch weather data!</AlertDescription>
-      </Alert> */}
-      <Box p={4} bg="gray.200" borderWidth="1px" borderRadius="xl" mt={2} mb={4} display={{ base: 'flex', md: 'block' }} flexDirection={{ base: 'column', md: 'row' }} justifyContent="space-around" alignItems="center">
-        <Flex flexDirection="column" alignItems="center" mb={{ base: 2, md: 0 }}>
-          <Text fontSize="lg">Current Weather</Text>
+    <Box p={4} bg="gray.200" borderWidth="1px" borderRadius="xl" mt={4}>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box flex="1" textAlign="center">
+          <Image
+            src={
+              weatherIcon[state.weather?.currentConditions.icon] ||
+              weatherIcon["cloudy"]
+            }
+            alt={state.weather?.currentConditions.icon}
+            boxSize="20"
+            objectFit="contain"
+          />
+        </Box>
+        <Box flex="2">
+          <Text fontSize="lg" fontWeight="bold">
+            Current Weather
+          </Text>
           <Text>{state.weather?.timezone}</Text>
           <Text>{new Date(Date.now()).toDateString()}</Text>
-        </Flex>
-        <Image src={weatherIcon[state.weather?.currentConditions.icon] || weatherIcon['cloudy']} alt={state.weather?.currentConditions.icon} boxSize="20" objectFit="contain" />
-        <Flex flexDirection="row" alignItems="center" justifyContent="center" mt={{ base: 2, md: 0 }}>
-          <Text fontSize="5xl" fontWeight="medium" color="teal.500">
-            {isNaN(tempCalc(state.weather?.currentConditions.temp)) ? 0 : tempCalc(state.weather?.currentConditions.temp)}
+        </Box>
+        <Box flex="2" textAlign="center">
+          <Text fontSize="5xl" fontWeight="bold" color="teal.500">
+            {isNaN(tempCalc(state.weather?.currentConditions.temp))
+              ? 0
+              : tempCalc(state.weather?.currentConditions.temp)}
             &#176;C
           </Text>
-          <Flex flexDirection="column" alignItems="center" ml={2}>
-            <Text>{state.weather?.currentConditions.conditions}</Text>
-            <Text fontSize="md" fontWeight="bold" color="teal.500">
-              {isNaN(tempCalc(state.weather?.days[0].tempmin)) ? 0 : tempCalc(state.weather?.days[0].tempmin)}
-              &#176;C
-            </Text>
-            <Text fontSize="md" fontWeight="bold" color="teal.500">
-              {isNaN(tempCalc(state.weather?.days[0].tempmax)) ? 0 : tempCalc(state.weather?.days[0].tempmax)}
-              &#176;C
-            </Text>
-          </Flex>
-        </Flex>
-        <Flex flexDirection="column" alignItems="center">
+          <Text>{state.weather?.currentConditions.conditions}</Text>
+        </Box>
+      </Flex>
+      <Flex justifyContent="space-between" alignItems="center" mt={4}>
+        <Box flex="1" textAlign="center">
           <Text fontSize="md" fontWeight="medium">
             Wind
           </Text>
-          <Text fontSize="lg" fontWeight="bold" color="teal.500">
-            {state.weather?.currentConditions.windspeed}km/h
-          </Text>
-        </Flex>
-        <Flex flexDirection="column" alignItems="center">
+          <Flex alignItems="center">
+            <WiStrongWind size={24} />
+            <Text fontSize="lg" fontWeight="bold" color="teal.500">
+              {state.weather?.currentConditions.windspeed}km/h
+            </Text>
+          </Flex>
+        </Box>
+        <Box flex="1" textAlign="center">
           <Text fontSize="md" fontWeight="medium">
             Humidity
           </Text>
-          <Text fontSize="lg" fontWeight="bold" color="teal.500">
-            {state.weather?.currentConditions?.humidity?.toFixed()}%
-          </Text>
-        </Flex>
-        <Flex flexDirection="column" alignItems="center">
+          <Flex alignItems="center">
+            <WiHumidity size={24} />
+            <Text fontSize="lg" fontWeight="bold" color="teal.500">
+              {state.weather?.currentConditions?.humidity?.toFixed()}%
+            </Text>
+          </Flex>
+        </Box>
+        <Box flex="1" textAlign="center">
           <Text fontSize="md" fontWeight="medium">
             Visibility
           </Text>
-          <Text fontSize="lg" fontWeight="bold" color="teal.500">
-            {state.weather?.currentConditions?.visibility?.toFixed()}km
-          </Text>
-        </Flex>
-      </Box>
-    </>
+          <Flex alignItems="center">
+            <WiCloudy size={24} />
+            <Text fontSize="lg" fontWeight="bold" color="teal.500">
+              {state.weather?.currentConditions?.visibility?.toFixed()}km
+            </Text>
+          </Flex>
+        </Box>
+      </Flex>
+      {/* Uncomment this section if you want to display an error message */}
+      {/* <Alert status="error" mt={4}>
+        <AlertIcon />
+        <AlertDescription>Could not fetch weather data!</AlertDescription>
+      </Alert> */}
+    </Box>
   );
 }
