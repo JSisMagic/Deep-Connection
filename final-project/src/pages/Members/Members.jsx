8b4c6af9b97dd3a837b5dfad5722396007ff8da8
gerganadq <gerganaquievy@gmail.com>
2023-09-05 20:47:23 +0300

    Improved UX by adding search icon in Members comp

--- final-project/src/pages/Members/Members.jsx
+++ final-project/src/pages/Members/Members.jsx
@@ -1,18 +1,24 @@
-import { Box, Input, Stack } from "@chakra-ui/react"
-import MembersList from "../../components/MembersList/MembersList"
+import { SearchIcon } from "@chakra-ui/icons"
+import { Box, Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
 import { useState } from "react"
+import MembersList from "../../components/MembersList/MembersList"
 
 const MembersPage = () => {
   const [searchTerm, setSearchTerm] = useState("")
 
   return (
     <Box p={5} paddingInline={10}>
-      <Input
-        type="text"
-        placeholder="search members by username or phone number"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value.toLowerCase())}
-      />
+      <InputGroup>
+        <Input
+          type="text"
+          placeholder="search members by username or phone number"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value.toLowerCase())}
+        />
+        <InputLeftElement>
+          <SearchIcon />
+        </InputLeftElement>
+      </InputGroup>
       <MembersList searchTerm={searchTerm} />
     </Box>
   )
