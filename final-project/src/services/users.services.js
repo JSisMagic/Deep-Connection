d2bc38561c6310501509ab9ce0bfc77a789523a6
gerganadq <gerganaquievy@gmail.com>
2023-09-05 21:36:29 +0300

    Add Firebase Storage Integration for Image Upload in users.services.js

--- final-project/src/services/users.services.js
+++ final-project/src/services/users.services.js
@@ -1,5 +1,7 @@
 import { equalTo, set, get, orderByChild, query, ref, update, remove } from "firebase/database"
-import { db } from "../config/firebase"
+import { getDownloadURL, uploadBytes, ref as storageRef } from "firebase/storage"
+import { db, storage } from "../config/firebase"
+import { v4 } from "uuid"
 
 export const getUser = async username => {
   const snapshot = await get(ref(db, `users/${username}`))
@@ -21,7 +23,7 @@ export const createUser = async (data = {}) => {
     ...data,
     contactLists: {},
     todos: {},
-  });
+  })
 
   return { ...data }
 }
@@ -30,6 +32,14 @@ export const getUserByUid = async uid => {
   const snapshot = await get(query(ref(db, "users"), orderByChild("uid"), equalTo(uid)))
   const value = snapshot.val()
   return value ? Object.values(value)[0] : null
+}
+
+export const uploadImage = async img => {
+  const imageRef = storageRef(storage, `images/${v4()}`)
+  const result = await uploadBytes(imageRef, img)
+  const url = await getDownloadURL(result.ref)
+
+  return url
 }
 
 export const updateUser = async (uid, data) => {
@@ -43,35 +53,35 @@ export const updateUser = async (uid, data) => {
   return { ...data }
 }
 
-export const getUserContactLists = async (uid) => {
-  const contactListsRef = ref(db, `users/${uid}/contactLists`);
-  const snapshot = await get(contactListsRef);
-  const result = {};
+export const getUserContactLists = async uid => {
+  const contactListsRef = ref(db, `users/${uid}/contactLists`)
+  const snapshot = await get(contactListsRef)
+  const result = {}
 
   Object.values(snapshot.val() || {}).map(value => {
     result[value.id] = {
       ...value,
-      contacts: value.contacts !== undefined ? value.contacts : {}
+      contacts: value.contacts !== undefined ? value.contacts : {},
     }
   })
 
   // debugger;
-  return result;
+  return result
 }
 
 export const getAllUsers = async () => {
-  const snapshot = await get(ref(db, "users"));
-  const value = snapshot.val();
+  const snapshot = await get(ref(db, "users"))
+  const value = snapshot.val()
 
-  return Object.values(value);
+  return Object.values(value)
 }
 
 export const createContactListForUser = async (uid, contactList) => {
-  const id = new Date().getTime().toString();
-  const list = { id, ...contactList };
+  const id = new Date().getTime().toString()
+  const list = { id, ...contactList }
 
-  await set(ref(db, `users/${uid}/contactLists/${id}`), list);
-  return { ...list };
+  await set(ref(db, `users/${uid}/contactLists/${id}`), list)
+  return { ...list }
 }
 
 export const updateContactListForUser = async (uid, listId, list) => {
@@ -80,11 +90,11 @@ export const updateContactListForUser = async (uid, listId, list) => {
 }
 
 export const deleteContactListForUser = async (uid, listId) => {
-  await remove(ref(db, `users/${uid}/contactLists/${listId}`));
+  await remove(ref(db, `users/${uid}/contactLists/${listId}`))
 }
 
 export const getUserByEmail = async email => {
-  const snapshot = await get(query(ref(db, "users"), orderByChild("email"), equalTo(email)));
-  const value = snapshot.val();
-  return value ? Object.values(value)[0] : null;
+  const snapshot = await get(query(ref(db, "users"), orderByChild("email"), equalTo(email)))
+  const value = snapshot.val()
+  return value ? Object.values(value)[0] : null
 }
