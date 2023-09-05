aaee9eb0b3fc5512aef2bddd1d0a98f659e46b98
gerganadq <gerganaquievy@gmail.com>
2023-09-05 17:45:39 +0300

    UI: Refactor weather component style

--- final-project/src/components/Weather/Weather.jsx
+++ final-project/src/components/Weather/Weather.jsx
@@ -53,8 +53,18 @@ export default function Weather() {
     return ((val - 32) / 1.8).toFixed();
   };
 
+  const gradientBackground = {
+    background: "linear-gradient(to bottom, #FFC0CB, #FF69B4)",
+  };
+
   return (
-    <Box p={4} bg="gray.200" borderWidth="1px" borderRadius="xl" mt={4}>
+    <Box
+      p={4}
+      borderRadius="lg"
+      mt={4}
+      boxShadow="md"
+      style={gradientBackground}
+    >
       <Flex justifyContent="space-between" alignItems="center">
         <Box flex="1" textAlign="center">
           <Image
@@ -63,58 +73,64 @@ export default function Weather() {
               weatherIcon["cloudy"]
             }
             alt={state.weather?.currentConditions.icon}
-            boxSize="20"
+            boxSize="50px"
             objectFit="contain"
           />
         </Box>
         <Box flex="2">
-          <Text fontSize="lg" fontWeight="bold">
+          <Text fontSize="xl" fontWeight="bold" textAlign="center">
             Current Weather
           </Text>
-          <Text>{state.weather?.timezone}</Text>
-          <Text>{new Date(Date.now()).toDateString()}</Text>
+          <Text color="gray.600" fontSize="sm" textAlign="center">
+            {state.weather?.timezone}
+          </Text>
+          <Text color="gray.600" fontSize="sm" textAlign="center">
+            {new Date(Date.now()).toLocaleDateString()}
+          </Text>
         </Box>
         <Box flex="2" textAlign="center">
-          <Text fontSize="5xl" fontWeight="bold" color="teal.500">
+          <Text fontSize="4xl" fontWeight="bold" color="teal.500">
             {isNaN(tempCalc(state.weather?.currentConditions.temp))
               ? 0
               : tempCalc(state.weather?.currentConditions.temp)}
             &#176;C
           </Text>
-          <Text>{state.weather?.currentConditions.conditions}</Text>
+          <Text color="gray.600" fontSize="md" textAlign="center">
+            {state.weather?.currentConditions.conditions}
+          </Text>
         </Box>
       </Flex>
       <Flex justifyContent="space-between" alignItems="center" mt={4}>
         <Box flex="1" textAlign="center">
-          <Text fontSize="md" fontWeight="medium">
+          <Text fontSize="md" fontWeight="medium" textAlign="center">
             Wind
           </Text>
-          <Flex alignItems="center">
-            <WiStrongWind size={24} />
+          <Flex alignItems="center" mt={1}>
+            <WiStrongWind size={36} color="pink.500" />
             <Text fontSize="lg" fontWeight="bold" color="teal.500">
-              {state.weather?.currentConditions.windspeed}km/h
+              {state.weather?.currentConditions.windspeed} km/h
             </Text>
           </Flex>
         </Box>
         <Box flex="1" textAlign="center">
-          <Text fontSize="md" fontWeight="medium">
+          <Text fontSize="md" fontWeight="medium" textAlign="center">
             Humidity
           </Text>
-          <Flex alignItems="center">
-            <WiHumidity size={24} />
+          <Flex alignItems="center" mt={1}>
+            <WiHumidity size={36} color="pink.500" />
             <Text fontSize="lg" fontWeight="bold" color="teal.500">
               {state.weather?.currentConditions?.humidity?.toFixed()}%
             </Text>
           </Flex>
         </Box>
         <Box flex="1" textAlign="center">
-          <Text fontSize="md" fontWeight="medium">
+          <Text fontSize="md" fontWeight="medium" textAlign="center">
             Visibility
           </Text>
-          <Flex alignItems="center">
-            <WiCloudy size={24} />
+          <Flex alignItems="center" mt={1}>
+            <WiCloudy size={36} color="pink.500" />
             <Text fontSize="lg" fontWeight="bold" color="teal.500">
-              {state.weather?.currentConditions?.visibility?.toFixed()}km
+              {state.weather?.currentConditions?.visibility?.toFixed()} km
             </Text>
           </Flex>
         </Box>
