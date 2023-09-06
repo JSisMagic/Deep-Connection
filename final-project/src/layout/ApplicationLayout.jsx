8e72876609868d2be3e9a031270b2b6099f56fa6
gerganadq <gerganaquievy@gmail.com>
2023-09-06 19:57:06 +0300

    Update ApplicationLayout Scroll Behavior

--- final-project/src/layout/ApplicationLayout.jsx
+++ final-project/src/layout/ApplicationLayout.jsx
@@ -5,11 +5,11 @@ import { HEADER_HEIGHT } from "../common/constrants"
 
 const ApplicationLayout = ({ children, notificationCount }) => {
   return (
-    <Flex height="100vh" w={{ base: "fit-content", sm: "full" }}>
+    <Flex height="100vh" overflowY="hidden" w={{ base: "fit-content", sm: "full" }}>
       <Sidebar notificationCount={notificationCount}/>
       <Box position="relative" flexGrow={1} height="100vh">
         <PrivateHeader />
-        <Box overflowY="hidden" height={`calc(100vh - ${HEADER_HEIGHT})`} position="relative">
+        <Box overflowY="auto" height={`calc(100vh - ${HEADER_HEIGHT})`} position="relative">
           {children}
         </Box>
       </Box>
