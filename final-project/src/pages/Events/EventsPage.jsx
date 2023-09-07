8d26285cc12485e7b37a1832d6ff54a48fb3fa28
gerganadq <gerganaquievy@gmail.com>
2023-09-07 20:26:49 +0300

    User UID Refactoring in EventsPage.jsx

--- final-project/src/pages/Events/EventsPage.jsx
+++ final-project/src/pages/Events/EventsPage.jsx
@@ -24,7 +24,7 @@ const EventsPage = () => {
           <PrivateEvents />
         </TabPanel>
         <TabPanel>
-          <MyEvents uid={user} />
+          <MyEvents uid={user?.uid} />
         </TabPanel>
       </TabPanels>
     </Tabs>
