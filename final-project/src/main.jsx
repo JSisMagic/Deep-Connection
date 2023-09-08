5c5a0853b808101226c10d490b8e54fbb2e1cb17
gerganadq <gerganaquievy@gmail.com>
2023-09-08 20:56:39 +0300

    Refactor: Simplified Component Wrapping

--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -12,13 +12,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <AuthContextProvider>
-        <AppContextProvider>
-          <BrowserRouter>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <AppContextProvider>
             <App />
-          </BrowserRouter>
-        </AppContextProvider>
-      </AuthContextProvider>
+          </AppContextProvider>
+        </AuthContextProvider>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
