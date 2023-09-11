57f716e5fcc4f8a098227550284e1b1dcb108c70
gerganadq <gerganaquievy@gmail.com>
2023-09-11 19:31:02 +0300

    Refactor: Integrate TodoComponent into DayView

--- final-project/src/components/Calendar/DayView.jsx
+++ final-project/src/components/Calendar/DayView.jsx
@@ -1,101 +1,111 @@
-import { Flex, Box, Grid, Button, IconButton, Heading } from "@chakra-ui/react";
-import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { getEventsForUser } from "../../services/event.services";
-import EventsColumn from "./EventsColumn";
-import HoursColumn from "./HoursColumn";
-import { addDays, format, subDays } from "date-fns";
-import { CALENDAR_HEIGHT } from "../../common/constrants";
+import { Flex, Box, Grid, Button, IconButton, Heading, GridItem, Image } from "@chakra-ui/react"
+import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../../context/AuthContext"
+import { getEventsForUser } from "../../services/event.services"
+import EventsColumn from "./EventsColumn"
+import HoursColumn from "./HoursColumn"
+import { addDays, format, subDays } from "date-fns"
+import { CALENDAR_HEIGHT } from "../../common/constrants"
+import TodoComponent from "../Todo/ToDo"
 
 const DayView = ({ date, setDate, onOpenDetailedModal }) => {
-  const { user } = useContext(AuthContext);
-  const [events, setEvents] = useState([]);
+  const { user } = useContext(AuthContext)
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
     if (user.uid) {
-      getEventsForUser(user.uid).then(setEvents).catch(console.error);
+      getEventsForUser(user.uid).then(setEvents).catch(console.error)
     }
-  }, [date, user.uid]);
+  }, [date, user.uid])
 
-  const handleNavigate = (action) => {
+  const handleNavigate = action => {
     return () => {
       switch (action) {
         case "today":
-          setDate(new Date());
-          break;
+          setDate(new Date())
+          break
         case "prev":
-          setDate((prev) => subDays(prev, 1));
-          break;
+          setDate(prev => subDays(prev, 1))
+          break
         case "next":
-          setDate((prev) => addDays(prev, 1));
+          setDate(prev => addDays(prev, 1))
       }
-    };
-  };
+    }
+  }
+
+  const DESKTOP_AREAS = `
+  "calendar quote"
+  "calendar todos"
+  `
+  const MOBILE_AREAS = `
+  "calendar"
+  "todos"
+  "quote"
+  `
 
   return (
-    <>
-      <Grid
-        templateColumns={{ base: "1fr", sm: "repeat(3, 1fr)" }}
-        py={2}
-        borderBottom="1px solid"
-        borderColor="gray.200"
-        gap={{ base: 2, md: 3 }}
-      >
-        <Button
-          onClick={handleNavigate("today")}
-          width="max-content"
-          mx={{ base: "auto", sm: "unset" }}
+    <Grid
+      templateAreas={{ base: MOBILE_AREAS, lg: DESKTOP_AREAS }}
+      gridTemplateColumns={{ lg: "2fr 1fr" }}
+      gridTemplateRows={{ lg: "1fr 2fr" }}
+      columnGap={3}
+      rowGap={3}
+    >
+      <GridItem area="calendar">
+        <Grid
+          templateColumns={{ base: "1fr", sm: "repeat(3, 1fr)" }}
+          py={2}
+          borderBottom="1px solid"
+          borderColor="gray.200"
+          gap={{ base: 2, md: 3 }}
         >
-          Today
-        </Button>
-
-        <Flex
-          justify="center"
-          align="center"
-          gap={3}
-          mx={{ base: "auto", sm: "unset" }}
-        >
-          <IconButton
-            size="sm"
-            icon={<ArrowBackIcon />}
-            onClick={handleNavigate("prev")}
-          />
-          <Heading
-            size="md"
-            textAlign="center"
-            w={{ base: "200px", md: "300px" }}
+          <Button
+            onClick={handleNavigate("today")}
+            width="max-content"
+            mx={{ base: "auto", sm: "unset" }}
           >
-            {/* {format(date, `${days[0].getDate()}-${days[6].getDate()}, MMMM`)} */}
-            {format(date, "PPPP")}
-          </Heading>
-          <IconButton
-            size="sm"
-            icon={<ArrowForwardIcon />}
-            onClick={handleNavigate("next")}
-          />
-        </Flex>
-      </Grid>
-      <Box overflowY="scroll" height={{ base: "300px", md: CALENDAR_HEIGHT }}>
-        <Flex
-          direction={{ base: "column", md: "row" }}
-          align={{ base: "stretch", md: "flex-start" }}
-        >
-          <HoursColumn />
-          <EventsColumn
-            date={new Date(date)}
-            borderLeft={true}
-            events={events}
-            onOpenDetailedModal={onOpenDetailedModal}
-          />
-        </Flex>
-      </Box>
-      <div>
-        <img src="https://zenquotes.io/api/image" alt="Random quote" />
-      </div>
-      ;
-    </>
-  );
-};
+            Today
+          </Button>
 
-export default DayView;
+          <Flex justify="center" align="center" gap={3} mx={{ base: "auto", sm: "unset" }}>
+            <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
+            <Heading size="md" textAlign="center" w={{ base: "200px", md: "300px" }}>
+              {format(date, "PPPP")}
+            </Heading>
+            <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
+          </Flex>
+        </Grid>
+        <Box overflowY="scroll" height={{ base: "300px", md: CALENDAR_HEIGHT }}>
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            align={{ base: "stretch", md: "flex-start" }}
+          >
+            <HoursColumn />
+            <EventsColumn
+              date={new Date(date)}
+              borderLeft={true}
+              events={events}
+              onOpenDetailedModal={onOpenDetailedModal}
+            />
+          </Flex>
+        </Box>
+      </GridItem>
+      <GridItem
+        h={{base: "250px", lg: "100%"}}
+        area="quote"
+        style={{
+          backgroundImage: `url(https://zenquotes.io/api/image)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      ></GridItem>
+      <GridItem area="todos">
+        <TodoComponent />
+      </GridItem>
+    </Grid>
+  )
+}
+
+export default DayView
