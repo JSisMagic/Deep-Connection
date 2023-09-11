2535ee9fc0917a2d25c5cad5d73b36b3bf121804
gerganadq <gerganaquievy@gmail.com>
2023-09-11 18:56:59 +0300

    Add quotes initiation to DayView

--- final-project/src/components/Calendar/DayView.jsx
+++ final-project/src/components/Calendar/DayView.jsx
@@ -1,66 +1,79 @@
-import { Flex, Box, Grid, Button, IconButton, Heading } from "@chakra-ui/react"
-import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons"
-import { useContext, useEffect, useState } from "react"
-import { AuthContext } from "../../context/AuthContext"
-import { getEventsForUser } from "../../services/event.services"
-import EventsColumn from "./EventsColumn"
-import HoursColumn from "./HoursColumn"
-import { addDays, format, subDays } from "date-fns"
-import { CALENDAR_HEIGHT } from "../../common/constrants"
+import { Flex, Box, Grid, Button, IconButton, Heading } from "@chakra-ui/react";
+import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { getEventsForUser } from "../../services/event.services";
+import EventsColumn from "./EventsColumn";
+import HoursColumn from "./HoursColumn";
+import { addDays, format, subDays } from "date-fns";
+import { CALENDAR_HEIGHT } from "../../common/constrants";
 
 const DayView = ({ date, setDate, onOpenDetailedModal }) => {
-  const { user } = useContext(AuthContext)
-  const [events, setEvents] = useState([])
+  const { user } = useContext(AuthContext);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     if (user.uid) {
-      getEventsForUser(user.uid).then(setEvents).catch(console.error)
+      getEventsForUser(user.uid).then(setEvents).catch(console.error);
     }
-  }, [date, user.uid])
+  }, [date, user.uid]);
 
-  const handleNavigate = action => {
+  const handleNavigate = (action) => {
     return () => {
       switch (action) {
         case "today":
-          setDate(new Date())
-          break
+          setDate(new Date());
+          break;
         case "prev":
-          setDate(prev => subDays(prev, 1))
-          break
+          setDate((prev) => subDays(prev, 1));
+          break;
         case "next":
-          setDate(prev => addDays(prev, 1))
+          setDate((prev) => addDays(prev, 1));
       }
-    }
-  }
+    };
+  };
 
   return (
     <>
       <Grid
-        templateColumns={{ base: "1fr", sm: "repeat(3, 1fr)" }} 
-        py={2} 
-        borderBottom="1px solid" 
+        templateColumns={{ base: "1fr", sm: "repeat(3, 1fr)" }}
+        py={2}
+        borderBottom="1px solid"
         borderColor="gray.200"
         gap={{ base: 2, md: 3 }}
       >
-        <Button 
-          onClick={handleNavigate("today")} 
-          width="max-content" 
-          mx={{ base: 'auto', sm: 'unset' }}>
+        <Button
+          onClick={handleNavigate("today")}
+          width="max-content"
+          mx={{ base: "auto", sm: "unset" }}
+        >
           Today
         </Button>
 
-        <Flex 
-          justify="center" 
-          align="center" 
-          gap={3} 
-          mx={{ base: 'auto', sm: 'unset' }}
+        <Flex
+          justify="center"
+          align="center"
+          gap={3}
+          mx={{ base: "auto", sm: "unset" }}
         >
-          <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
-          <Heading  size="md" textAlign="center"  w={{ base: "200px", md: "300px" }} >
+          <IconButton
+            size="sm"
+            icon={<ArrowBackIcon />}
+            onClick={handleNavigate("prev")}
+          />
+          <Heading
+            size="md"
+            textAlign="center"
+            w={{ base: "200px", md: "300px" }}
+          >
             {/* {format(date, `${days[0].getDate()}-${days[6].getDate()}, MMMM`)} */}
             {format(date, "PPPP")}
           </Heading>
-          <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
+          <IconButton
+            size="sm"
+            icon={<ArrowForwardIcon />}
+            onClick={handleNavigate("next")}
+          />
         </Flex>
       </Grid>
       <Box overflowY="scroll" height={{ base: "300px", md: CALENDAR_HEIGHT }}>
@@ -77,8 +90,12 @@ const DayView = ({ date, setDate, onOpenDetailedModal }) => {
           />
         </Flex>
       </Box>
+      <div>
+        <img src="https://zenquotes.io/api/image" alt="Random quote" />
+      </div>
+      ;
     </>
-  )
-}
+  );
+};
 
-export default DayView
+export default DayView;
