96980a93c3d4cfe127a2306410b8441dae8f56d0
gerganadq <gerganaquievy@gmail.com>
2023-09-12 18:53:51 +0300

    Refactor EventCart

--- final-project/src/components/EventCard/EventCard.jsx
+++ final-project/src/components/EventCard/EventCard.jsx
@@ -1,20 +1,47 @@
-import { Box, Button, Flex, Heading, Icon, Image, Stack, Text, useToast } from "@chakra-ui/react"
-import { format } from "date-fns"
-import { FaLocationDot } from "react-icons/fa6"
-import FallbackImg from "../../assets/images/event.jpg"
-import { useContext } from "react"
-import { AuthContext } from "../../context/AuthContext"
-import { joinOrLeaveEvent } from "../../services/event.services"
-import { eventActions } from "../../common/event-enums"
-import { useState } from "react"
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Image,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { format } from "date-fns";
+import { FaLocationDot } from "react-icons/fa6";
+import FallbackImg from "../../assets/images/event.jpg";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { joinOrLeaveEvent } from "../../services/event.services";
+import { eventActions } from "../../common/event-enums";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const EventPageCard = ({ eventData, onOpenDetailedEvent }) => {
-  const { userData } = useContext(AuthContext)
+const EventPageCard = ({
+  eventData,
+  onOpenDetailedEvent,
+  isUsedLanding = false,
+}) => {
+  const { userData } = useContext(AuthContext);
   const toast = useToast();
-  const [hasJoined, setHasJoined] = useState(userData?.events?.[eventData.id])
+  const navigate = useNavigate();
+  const [hasJoined, setHasJoined] = useState(userData?.events?.[eventData.id]);
+
+  const handleClickEvent = () => {
+    if (userData === null) {
+      return navigate("/login");
+    }
+    onOpenDetailedEvent(eventData);
+  };
 
   const handleJoinOrLeave = async (event) => {
     event.stopPropagation();
+
+    if (userData === null) {
+      return navigate("/login");
+    }
 
     try {
       await joinOrLeaveEvent(
@@ -43,35 +70,51 @@ const EventPageCard = ({ eventData, onOpenDetailedEvent }) => {
         isClosable: true,
       });
     }
-  }
-  
-  console.log(eventData, hasJoined)
+  };
+
   return (
     <Flex
       p={3}
-      direction={{ base: "column", lg: "row" }}
-      h={{ base: "max-content", lg: "25vh" }}
+      direction={{ base: "column", lg: isUsedLanding ? "column" : "row" }}
+      h={{ base: "max-content", lg: isUsedLanding ? "100%" : "25vh" }}
       align="center"
       width="100%"
       gap={8}
       bg="gray.50"
-      borderRadius="md"
+      borderRadius={isUsedLanding ? "xl" : "md"}
       cursor="pointer"
-      maxW="1500px"
+      maxW={{ lg: isUsedLanding ? "500px" : "1500px" }}
       marginInline="auto"
-      onClick={() => onOpenDetailedEvent(eventData)}
+      transition="all .2s linear"
+      _hover={{
+        transform: "scale(1.02)",
+        boxShadow: "0px 2px 6px rgba(0,0,0,.2)",
+      }}
+      onClick={handleClickEvent}
     >
-      <Stack alignSelf={{ lg: "start" }} align="center" width="10%">
-        <Text fontWeight={500}>{format(eventData.startDate, "ccc").toUpperCase()}</Text>
+      <Stack
+        alignSelf={{ lg: !isUsedLanding && "start" }}
+        align="center"
+        width="10%"
+      >
+        <Text fontWeight={500}>
+          {format(eventData.startDate, "ccc").toUpperCase()}
+        </Text>
         <Heading size="md">{format(eventData.startDate, "d")}</Heading>
       </Stack>
-      <Stack h="100%" justify="space-between" flexGrow={1} width={{ lg: "60%" }}>
+      <Stack
+        h="100%"
+        justify="space-between"
+        flexGrow={1}
+        width={{ lg: isUsedLanding ? "90%" : "60%" }}
+      >
         <Flex justify="space-between" align="center">
           <Text fontWeight={500}>
-            {format(eventData.startDate, "MMMM")} @{format(eventData.startDate, "HH:mm")} -
+            {format(eventData.startDate, "MMMM")} @
+            {format(eventData.startDate, "HH:mm")} -
             {format(eventData.endDate, "HH:mm O")}
           </Text>
-          {userData.uid !== eventData.creatorId && (
+          {userData?.uid !== eventData.creatorId && (
             <Button colorScheme="blue" onClick={handleJoinOrLeave}>
               {hasJoined ? "Leave" : "Join"}
             </Button>
@@ -84,9 +127,15 @@ const EventPageCard = ({ eventData, onOpenDetailedEvent }) => {
             <Text fontWeight={600}>{eventData.location}</Text>
           </Flex>
         )}
-        <Text dangerouslySetInnerHTML={{ __html: eventData.description }} overflow="hidden" />
+        <Text
+          dangerouslySetInnerHTML={{ __html: eventData.description }}
+          overflow="hidden"
+        />
       </Stack>
-      <Box h="100%" w={{ lg: "30%" }}>
+      <Box
+        h={{ base: "100%", lg: isUsedLanding ? "30vh" : "100%" }}
+        w={{ lg: isUsedLanding ? "100%" : "30%" }}
+      >
         <Image
           h="100%"
           w="100%"
@@ -96,7 +145,7 @@ const EventPageCard = ({ eventData, onOpenDetailedEvent }) => {
         />
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default EventPageCard
+export default EventPageCard;
