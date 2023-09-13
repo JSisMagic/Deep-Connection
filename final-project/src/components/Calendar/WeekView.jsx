d58103298efed7d34b45d3dd8fae17b4351fb6ec
gerganadq <gerganaquievy@gmail.com>
2023-09-13 21:12:19 +0300

    Updated Grid Template Columns for Calendar View

--- final-project/src/components/Calendar/WeekView.jsx
+++ final-project/src/components/Calendar/WeekView.jsx
@@ -9,7 +9,7 @@ import { addDaysToDate, getStartOfWeek } from "../../services/calendar.services"
 import { getEventsForUser } from "../../services/event.services"
 import EventsColumn from "./EventsColumn"
 import HoursColumn from "./HoursColumn"
-import { CALENDAR_HEIGHT } from "../../common/constrants"
+import { CALENDAR_HEIGHT, HEADER_HEIGHT } from "../../common/constrants"
 
 const WeekView = ({ date, setDate, isWorkWeek = false, onOpenDetailedModal }) => {
   const { userData } = useContext(AuthContext)
@@ -46,43 +46,35 @@ const WeekView = ({ date, setDate, isWorkWeek = false, onOpenDetailedModal }) =>
   }
 
   return (
-    <>
-      <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} py={2} gap={{ base: 2, md: 3 }}>
-        <Button onClick={handleNavigate("today")} width="max-content" mx={{ base: 'auto', sm: 'unset' }}>
+    <Box>
+      <Grid templateColumns="repeat(3, 1fr)" py={2}>
+        <Button onClick={handleNavigate("today")} width="max-content">
           Today
         </Button>
 
-        <Flex 
-          justify="center" 
-          align="center" 
-          gap={3} 
-          mx={{ base: 'auto', sm: 'unset' }}
-        >
+        <Flex justify="center" align="center" gap={3}>
           <IconButton size="sm" icon={<ArrowBackIcon />} onClick={handleNavigate("prev")} />
-          <Heading 
-            w={{ base: "200px", md: "300px" }} 
-            size="md" 
-            textAlign="center"
-          >
+          <Heading w="300px" size="md" textAlign="center">
             {format(days[0], "PP")} - {format(days[days.length - 1], "PP")}
           </Heading>
           <IconButton size="sm" icon={<ArrowForwardIcon />} onClick={handleNavigate("next")} />
         </Flex>
       </Grid>
       <Grid
-        height={{ base: "500px", md: CALENDAR_HEIGHT }}
+        maxH={{ base: "100%", md: `calc(82vh - ${HEADER_HEIGHT})` }}
         overflowY="auto"
         templateRows="40px 1fr"
-        templateColumns={{ base: `repeat(${daysCount + 1}, 1fr)`, md: `50px repeat(${daysCount}, 1fr)` }}
-        border="1px solid"
+        templateColumns={`50px repeat(${daysCount}, 1fr)`}
+        borderLeft="1px solid"
+        borderRight="1px solid"
+        borderTop="1px solid"
         borderColor="gray.300"
-        // gap={{ base: 2, md: 3 }}
       >
         <GridItem rowStart={2} colEnd={2}>
           <HoursColumn />
         </GridItem>
         {days.map((day, i) => (
-          <GridItem key={day} colStart={{ base: i + 1, md: i + 2 }}>
+          <GridItem key={day} colStart={i + 2}>
             <Grid templateRows="40px 1fr" borderLeft="1px solid" borderColor="gray.300">
               <GridItem rowEnd={2} bgColor="gray.50">
                 <Center>{format(day, "E, do")}</Center>
@@ -99,7 +91,7 @@ const WeekView = ({ date, setDate, isWorkWeek = false, onOpenDetailedModal }) =>
           </GridItem>
         ))}
       </Grid>
-    </>
+    </Box>
   )
 }
 
