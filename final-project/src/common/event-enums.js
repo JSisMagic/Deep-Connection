e55dab0b7ee01ca8f9da0d69a9afed5e5524c293
gerganadq <gerganaquievy@gmail.com>
2023-09-13 18:30:00 +0300

    Add eventCategories in in event-enums

--- final-project/src/common/event-enums.js
+++ final-project/src/common/event-enums.js
@@ -2,3 +2,12 @@ export const eventActions = {
   join: "join",
   leave: "leave",
 }
+
+export const eventCategories = {
+  pink: "Pink",
+  purple: "Purple",
+  blue: "Blue",
+  green: "Green",
+  yellow: "Yellow",
+  orange: "Orange",
+}
